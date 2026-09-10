2208a622b88030b31dce497068680c7a808e1f52
dresseldj29-sudo <dresseldj29@gmail.com>
2026-09-10 19:38:48 +0200

    Update OWNER_EMAIL and OWNER_PASSWORD values

--- script.js
+++ script.js
@@ -31,9 +31,9 @@
    NICHT dein Gmail-Passwort!
    ========================================================= */
 
-const OWNER_EMAIL = "deine-email@gmail.com";
+const OWNER_EMAIL = "Owner";
 
-const OWNER_PASSWORD = "MeinOwnerPasswort123!";
+const OWNER_PASSWORD = "Server";
 
 
 /* =========================================================
