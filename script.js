b0a036e8dda1da5f8d2aa1e94ac9a74c9189295e
dresseldj29-sudo <dresseldj29@gmail.com>
2026-09-10 19:18:56 +0200

    Update owner email and password in script.js

--- script.js
+++ script.js
@@ -14,9 +14,9 @@
    Deshalb KEIN wichtiges echtes Passwort verwenden.
 ===================================================== */
 
-const OWNER_EMAIL = "deine-email@gmail.com";
+const OWNER_EMAIL = "ServerOwner@gmail.com";
 
-const OWNER_PASSWORD = "MeinTestPasswort123";
+const OWNER_PASSWORD = "Server";
 
 
 /* =====================================================
